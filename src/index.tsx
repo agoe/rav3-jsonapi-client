c328bb4eb8cbb4dd8ef232232772c10943693141
agoe <achim.goetz@gmail.com>
2021-02-03 16:46:33 +0100

    n

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  //  return <div className={styles.test}>Example Component: {text}</div>
+  return <div>Example Component: {text}</div>
 }
